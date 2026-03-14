88b4e6aebd3b720b620896ff6a66377881f80f5f
David <David.Piruzashvili@gmail.com>
2026-03-14 20:19:00 +0400

    Modified  db.js connection

--- src/config/db.js
+++ src/config/db.js
@@ -3,19 +3,23 @@ import mongoose from 'mongoose';
 dotenv.config();
 
 // Serverless connection for Vercel
-const MONGODB_URI = process.env.DB_URL;
-let cached = global.mongoose;
-
-if (!cached) {
-    cached = global.mongoose = { conn: null, promise: null };
+const MONGODB_URL = process.env.DB_URL;
+if (!MONGODB_URL) {
+    throw new Error("Please define MONGO_URL");
 }
+
+let cached = global.mongoose || { conn: null, promise: null };
+global.mongoose = cached;
+
 
 export async function connectDB() {
     if (cached.conn) return cached.conn;
 
     if (!cached.promise) {
-        cached.promise = mongoose.connect(MONGODB_URI, {
-            serverSelectionTimeoutMS: 30000
+        cached.promise = mongoose.connect(MONGODB_URL, {
+            serverSelectionTimeoutMS: 30000,
+            bufferCommands: true,
+            maxPoolSize: 10
         });
     }
 
